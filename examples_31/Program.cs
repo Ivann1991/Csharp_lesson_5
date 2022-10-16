075fef7f9d0be8756dab441b95ac8e40e0a51aa9
Ivan <Korneychik91@yandex.ru>
2022-10-16 22:06:03 +0300

    first commit

---
+++ examples_31/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив из 12 эелементов, заполненный случайными числами из промежутка [-9,9]. Найдите сумму отрицательных и положительных элемнтов массива.
+int[] array = GetArray(12, -9, 9);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+int negativeSum = GetNegativeSum(array);
+Console.WriteLine($"\nСумма отрицательных чисел равна {negativeSum}");
+
+int positiveSum = GetPositiveSum(array);
+Console.WriteLine($"\nСумма положительных чисел равна {positiveSum}");
+
+
+int GetPositiveSum(int[] array)
+{
+    int positiveSum = 0;
+    foreach(int el in array)
+    {
+        if(el>0) positiveSum+=el;
+    }
+    return positiveSum;
+}
+
+
+int GetNegativeSum(int[] array)
+{
+    int negativeSum = 0;
+    foreach(int el in array)
+    {
+        if(el<0) negativeSum+=el;
+    }
+    return negativeSum;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
---
+++ examples_32/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на отрицательные и наоборот.
+
+Console.Clear();
+
+int[] array = GetArray(10, -10, 10);
+Console.Write ("[");
+for (int i = 0; i < array.Length -1 ; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.Write($"{array[array.Length -1]}] ");
+Console.Write("--> ");
+int [] newarray = zamena(array);
+
+Console.Write ("[");
+for (int i = 0; i < newarray.Length -1 ; i++)
+{
+    Console.Write($"{newarray[i]} ");
+}
+Console.Write($"{newarray[newarray.Length -1]}] ");
+
+
+
+
+
+
+///////////////////////////////////////
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
+
+int[] zamena(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array [i] = array[i] * -1;
+    return array;
+}
+

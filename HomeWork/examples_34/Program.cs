f3367fea815dfd6c1db121a2696712083e4171d8
Ivan <Korneychik91@yandex.ru>
2022-10-17 21:53:19 +0300

    ex_34,ex_36 done

---
+++ HomeWork/examples_34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных чисел в массиве.
+
+Console.Clear();
+
+
+int [] newarray = GetArray(10, 100, 999);
+
+PrintArray(newarray);
+int even = CountEven(newarray);
+
+Console.WriteLine ("\nКоличество четных чисел в массиве = " + even);
+
+/////////////////////////
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"{array[array.Length - 1]}] ");
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count = count + 1;
+    }
+    return count;
+}
---
+++ HomeWork/examples_36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+Console.Clear();
+
+int[] newarray = GetArray(11, -100, 100);
+Console.Write("В сгенерированном массиве -> ");
+PrintArray(newarray);
+int a = SumUnevenPosition(newarray);
+Console.Write("cумма элементов нечетных позиций = " + a);
+
+/////////////////////////
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"{array[array.Length - 1]}] ");
+}
+
+int SumUnevenPosition(int[] array)
+{
+    int SumUneven = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+        SumUneven = array[i] + SumUneven;
+    return SumUneven;
+}
